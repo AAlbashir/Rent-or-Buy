2ea3c4cde590e05d460b79edfba1771587e4bfdf
Jeremy Roy <jmroy1@asu.edu>
2025-02-08 04:18:08 -0500

    Added selling_cost_rate parameter and updated total_buy_cost

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,4 @@
-def compare_rent_vs_buy(length_of_stay, monthly_rent, home_price, down_payment, mortgage_rate, investment_interest_rate, property_tax_rate=1.2, maintenance_rate=1.0): # Added new parameter...JMR
+def compare_rent_vs_buy(length_of_stay, monthly_rent, home_price, down_payment, mortgage_rate, investment_interest_rate, property_tax_rate=1.2, maintenance_rate=1.0, selling_cost_rate=8.0): # Added new parameters...JMR
     """
     Compare renting vs. buying a home.
     
@@ -10,6 +10,7 @@ def compare_rent_vs_buy(length_of_stay, monthly_rent, home_price, down_payment, 
     :param property_tax_rate: Annual property tax rate (default 1.2%)
     :param maintenance_rate: Annual maintenance cost rate (default 1.0%)
     :param investment_interest_rate: Annual investment interest (as percentage)  ...Added parameter...JMR
+    :param selling_cost_rate: Cumulative costs of selling a home (default 8.0%)...Added parameter...JMR
     :return: Total costs for renting and buying, and recommendation
     """
     import numpy as np
@@ -22,6 +23,8 @@ def compare_rent_vs_buy(length_of_stay, monthly_rent, home_price, down_payment, 
     loan_amount = home_price - down_payment
     monthly_interest_rate = (mortgage_rate / 100) / 12
     num_payments = length_of_stay * 12
+    home_appreciation = home_price * (1 + (investment_interest_rate / 100) / 12)**(12 * length_of_stay) # Added home appreciation value...JMR
+    selling_costs = home_appreciation * (selling_cost_rate / 100)    
     
     if monthly_interest_rate > 0:
         monthly_mortgage_payment = loan_amount * (monthly_interest_rate * (1 + monthly_interest_rate) ** num_payments) / ((1 + monthly_interest_rate) ** num_payments - 1)
@@ -31,8 +34,9 @@ def compare_rent_vs_buy(length_of_stay, monthly_rent, home_price, down_payment, 
     total_mortgage_cost = monthly_mortgage_payment * num_payments
     total_property_tax = (property_tax_rate / 100) * home_price * length_of_stay
     total_maintenance = (maintenance_rate / 100) * home_price * length_of_stay
+    total_resale_value = home_appreciation - selling_costs # Added a financial return estimate for sale of home...JMR
     
-    total_buy_cost = down_payment + total_mortgage_cost + total_property_tax + total_maintenance
+    total_buy_cost = down_payment + total_mortgage_cost + total_property_tax + total_maintenance - total_resale_value # Subtracted resale value of home from buying costs...JMR
     
     # Recommendation
     if total_rent_cost < total_buy_cost:
