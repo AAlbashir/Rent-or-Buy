4aa1f72c52836cf91c83808ea4105ce6344d471a
Jeremy Roy <jmroy1@asu.edu>
2025-02-09 13:15:15 -0500

    Added a second chart to the program and formatting

--- vcv.py
+++ vcv.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
 
 def plot_vacancy(vacancy):
     """
@@ -18,27 +19,49 @@ def plot_vacancy(vacancy):
     df["City"] = df["City"].str.replace("!!Estimate", "").str.strip()
      
     x_col = df.loc[:, "City"] # x-axis values
-    y1_col = df.loc[:, "Vacant housing units"] # Data for bar graph
+    y1_col = df.loc[:, "Vacant housing units"] # Data for bar chart 1
+    y2_col = df.loc[:, "Median House Value (dollars)"] # Data for bar chart 2
     buy_line = df.loc[:, "Median Buy Cost (dollars)"]
     rent_line = df.loc[:, "Median Rent Cost (dollars)"]
 
-    fig, ax1 = plt.subplots(figsize=(10, 10))
+    # Chart 1
+    fig, ax1 = plt.subplots(figsize=(10, 5))
+    plt.title("Vacancy & Cost")
     plt.xticks(rotation=85)
     plt.xlabel("City")
-    
-    # Primary y-axis
-    ax1.bar(x_col, y1_col, color='royalblue', label="Vacant housing units")
+        
+    # Primary y-axis - chart 1
+    ax1.bar(x_col, y1_col, color='slategrey', label="Vacant housing units")
     ax1.set_ylabel("Number Vacant", color='black')
     ax1.legend(loc="upper left")
+    ax1.yaxis.set_major_formatter(mticker.FuncFormatter(lambda x, pos: f"{int(x/1000)}K"))
                   
-    # Secondary y-axis
+    # Secondary y-axis - chart 1
     ax2 = ax1.twinx()
     ax2.plot(x_col, buy_line, color='forestgreen', marker='o', linestyle='-', label="Median buy cost")
     ax2.plot(x_col, rent_line, color='darkred', marker='o', linestyle='-', label="Median rent cost")
-    ax2.set_ylabel("Cost", color='black')
+    ax2.set_ylabel("Cost (USD)", color='black')
     ax2.legend(loc="best")
 
-    fig.tight_layout()
+    # Chart 2
+    fig, ax3 = plt.subplots(figsize=(10, 5))
+    plt.title("Home Value & Cost")
+    plt.xticks(rotation=85)
+    plt.xlabel("City")
+    
+    # Primary y-axis - chart 2
+    ax3.bar(x_col, y2_col, color='royalblue', label="Median home value")
+    ax3.set_ylabel("Home Value (USD)", color='black')
+    ax3.legend(loc="upper left")
+    ax3.yaxis.set_major_formatter(mticker.FuncFormatter(lambda x, pos: f"{int(x/1000)}K"))
+                  
+    # Secondary y-axis - chart 2
+    ax4 = ax3.twinx()
+    ax4.plot(x_col, buy_line, color='forestgreen', marker='o', linestyle='-', label="Median buy cost")
+    ax4.plot(x_col, rent_line, color='darkred', marker='o', linestyle='-', label="Median rent cost")
+    ax4.set_ylabel("Cost (USD)", color='black')
+    ax4.legend(loc="best")
+
     plt.show()
 
 vacancy = "data/USA DP04 vacancy value cost.csv" 
