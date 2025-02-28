c7f1fb8c5c976dc387886830284301e79f1a3429
Abdallah Albashir <aalbashi@asu.edu>
2025-02-28 16:35:11 +0300

    Add modified test codes to test Integrated code
    These test codes import the functions to be tested from the Basic_Features_Integrated

---
+++ test_calculator_in_Integrated.py
@@ -0,0 +1,59 @@
+import unittest
+from Basic_Features import compare_rent_vs_buy
+
+class TestCalculatorResult(unittest.TestCase):
+   
+    def test_rent_cheaper(self):
+        inputs = {
+            "length_of_stay": 1,  # years
+            "monthly_rent": 1,  # dollars
+            "home_price": 117.70,  # dollars
+            "down_payment": 0,  # dollars
+            "mortgage_rate": 0,  # percent
+            "investment_interest_rate": 0 # percent
+        }
+        result = compare_rent_vs_buy(**inputs)
+        expected = {
+            "Rent Cost": 12, 
+            "Buy Cost": 12.01, 
+            "Recommendation": 'Renting is financially better.'
+        }
+        self.assertEqual(result, expected)
+    
+    def test_buy_cheaper(self):
+        inputs = {
+            "length_of_stay": 1,  # years
+            "monthly_rent": 1,  # dollars
+            "home_price": 117.59,  # dollars
+            "down_payment": 0,  # dollars
+            "mortgage_rate": 0,  # percent
+            "investment_interest_rate": 0 # percent
+        }
+        result = compare_rent_vs_buy(**inputs)
+        expected = {
+            "Rent Cost": 12, 
+            "Buy Cost": 11.99, 
+            "Recommendation": 'Buying is financially better.'
+        }
+        self.assertEqual(result, expected)
+   
+    def test_equal_cost(self):
+      inputs = {
+            "length_of_stay": 1,  # years
+            "monthly_rent": 1,  # dollars
+            "home_price": 117.65,  # dollars
+            "down_payment": 0,  # dollars
+            "mortgage_rate": 0,  # percent
+            "investment_interest_rate": 0 # percent
+      }
+      result = compare_rent_vs_buy(**inputs)
+      expected = {
+            "Rent Cost": 12, 
+            "Buy Cost": 12, 
+            "Recommendation": 'Either option is equivalent.'
+      }
+      self.assertEqual(result, expected)
+
+# Added Name-Main idiom to run unit test as a script and automatically load unittest module
+if __name__== "__main__":
+    unittest.main()
